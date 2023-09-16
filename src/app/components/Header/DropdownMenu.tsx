5109af1737e80a53aeadd9aa215bebd88d2763e3
Vitalii Braun <vitossvx@gmail.com>
2023-09-16 14:25:05 +0300

    fix stile

--- src/app/components/Header/DropdownMenu.tsx
+++ src/app/components/Header/DropdownMenu.tsx
@@ -56,8 +56,8 @@ export function DropdownMenu({
         </svg>
       </button>
       {isCatOpen && (
-        <div className="absolute left-1/2 z-10 mt-5 flex w-screen max-w-max -translate-x-1/2 px-4">
-          <div className="w-screen max-w-md flex-auto overflow-hidden rounded-3xl bg-white text-sm leading-6 shadow-lg ring-1 ring-gray-900/5">
+        <div className="absolute left-1/2 z-10 mt-5 flex max-w-max -translate-x-1/2 px-4">
+          <div className="max-w-md flex-auto overflow-hidden rounded-3xl bg-white text-sm leading-6 shadow-lg ring-1 ring-gray-900/5">
             <div className="p-4">{children}</div>
           </div>
         </div>
