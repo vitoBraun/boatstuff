0907b1cb9b18f92eaf3b5458327d2d2ac10405d3
Vitalii Braun <vitossvx@gmail.com>
2023-09-23 17:32:24 +0300

    Fix gha

--- src/app/components/Catalog/Items.tsx
+++ src/app/components/Catalog/Items.tsx
@@ -7,14 +7,10 @@ export default function Items({ products }: { products: Product[] }) {
     return <div>NO ITEMS TO DISPLAY</div>;
   }
   return (
-
-
     <div className="grid lg:grid-cols-3 gap-x-5 gap-y-10 md:grid-cols-2 sm:grid-cols-1">
       {products.map((product) => (
         <Item product={product} />
       ))}
     </div>
-
-
   );
 }
