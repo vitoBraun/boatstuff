e8b99810ee43f9434cbb326a14faaebf286a9620
Vitalii Braun <vitossvx@gmail.com>
2023-09-23 11:07:32 +0300

    No chache on fetch

--- src/app/SSRHooks/useFetch.ts
+++ src/app/SSRHooks/useFetch.ts
@@ -1,7 +1,6 @@
 export const useFetch = (route: string) => {
   const url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/${route}`;
-  console.log(url);
-  const res = fetch(url).then((data) => data.json());
+  const res = fetch(url, { cache: "no-cache" }).then((data) => data.json());
 
   return res;
 };
